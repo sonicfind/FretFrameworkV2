6268bd57cf90d0cb4170cf006d864858693ff3f5
sonicfind <sonicfind2@gmail.com>
2023-04-21 09:16:12 -0500

    song_mid: formatting

--- FretFrameworkV2/Track Collection/Song_mid.cpp
+++ FretFrameworkV2/Track Collection/Song_mid.cpp
@@ -5,10 +5,8 @@
 void Song::load_mid(const std::filesystem::path& path)
 {
 	MidiFileReader reader(path);
-
 	while (reader.startNextTrack())
 	{
-		
 		if (reader.getTrackNumber() == 1)
 		{
 			if (reader.getEventType() == MidiEventType::Text_TrackName)
@@ -23,45 +21,36 @@ void Song::load_mid(const std::filesystem::path& path)
 			const std::string_view name = reader.extractTextOrSysEx();
 			if (name == "EVENTS")
 				load_events_midi(reader);
-			else
+			else if (name == "PART GUITAR" || name == "T1 GEMS")
+				m_noteTracks.lead_5.load(reader);
+			else if (name == "PART GUITAR GHL")
+				m_noteTracks.lead_6.load(reader);
+			else if (name == "PART BASS")
+				m_noteTracks.bass_5.load(reader);
+			else if (name == "PART BASS GHL")
+				m_noteTracks.bass_6.load(reader);
+			else if (name == "PART RHYTHM")
+				m_noteTracks.rhythm.load(reader);
+			else if (name == "PART GUITAR COOP")
+				m_noteTracks.coop.load(reader);
+			else if (name == "PART KEYS")
+				m_noteTracks.keys.load(reader);
+			else if (name == "PART DRUMS")
 			{
-				Track* track = nullptr;
-				if (name == "PART GUITAR" || name == "T1 GEMS")
-					m_noteTracks.lead_5.load(reader);
-				else if (name == "PART GUITAR GHL")
-					m_noteTracks.lead_6.load(reader);
-				else if (name == "PART BASS")
-					m_noteTracks.bass_5.load(reader);
-				else if (name == "PART BASS GHL")
-					m_noteTracks.bass_6.load(reader);
-				else if (name == "PART RHYTHM")
-					m_noteTracks.rhythm.load(reader);
-				else if (name == "PART GUITAR COOP")
-					m_noteTracks.coop.load(reader);
-				else if (name == "PART KEYS")
-					m_noteTracks.keys.load(reader);
-				else if (name == "PART DRUMS")
-				{
-					Legacy_DrumTrack drumsLegacy(reader);
-					if (drumsLegacy.getDrumType() != DrumType_Enum::FIVELANE)
-						drumsLegacy.transfer(m_noteTracks.drums4_pro);
-					else
-						drumsLegacy.transfer(m_noteTracks.drums5);
-				}
-				else if (name == "PART VOCALS")
-					m_noteTracks.vocals.load<0>(reader);
+				Legacy_DrumTrack drumsLegacy(reader);
+				if (drumsLegacy.getDrumType() != DrumType_Enum::FIVELANE)
+					drumsLegacy.transfer(m_noteTracks.drums4_pro);
 				else
-				{
-					if (name == "HARM1")
-						m_noteTracks.harmonies.load<0>(reader);
-					else if (name == "HARM2")
-						m_noteTracks.harmonies.load<1>(reader);
-					else if (name == "HARM3")
-						m_noteTracks.harmonies.load<2>(reader);
-					else
-						continue;
-				}
+					drumsLegacy.transfer(m_noteTracks.drums5);
 			}
+			else if (name == "PART VOCALS")
+				m_noteTracks.vocals.load<0>(reader);
+			else if (name == "HARM1")
+				m_noteTracks.harmonies.load<0>(reader);
+			else if (name == "HARM2")
+				m_noteTracks.harmonies.load<1>(reader);
+			else if (name == "HARM3")
+				m_noteTracks.harmonies.load<2>(reader);
 		}
 	}
 }
