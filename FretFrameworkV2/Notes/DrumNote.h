2eb4d9acf953162241d8649d2695019c9adc598c
sonicfind <sonicfind2@gmail.com>
2023-04-20 23:05:26 -0500

    DrumNote: additional conditions + fixed laning for modifier setters & getters

--- FretFrameworkV2/Notes/DrumNote.h
+++ FretFrameworkV2/Notes/DrumNote.h
@@ -183,16 +183,16 @@ public:
 	[[nodiscard]] bool isFlammed() const noexcept { return m_isFlammed; }
 
 public:
-	[[nodiscard]] bool isAccented(size_t lane) const noexcept { return this->m_colors[lane].getDyanmics() == DrumDynamics::Accent; }
-	[[nodiscard]] bool isGhosted(size_t lane) const noexcept { return this->m_colors[lane].getDyanmics() == DrumDynamics::Ghost; }
-	void setDynamics(size_t lane, DrumDynamics dyn) { this->m_colors[lane].setDynamics(dyn); }
+	[[nodiscard]] bool isAccented(size_t lane) const noexcept { return lane > 0 && this->m_colors[lane - 1].getDyanmics() == DrumDynamics::Accent; }
+	[[nodiscard]] bool isGhosted(size_t lane) const noexcept { return lane > 0 && this->m_colors[lane - 1].getDyanmics() == DrumDynamics::Ghost; }
+	void setDynamics(size_t lane, DrumDynamics dyn) { if (lane > 0) this->m_colors[lane - 1].setDynamics(dyn); }
 
 	template <typename = std::enable_if<PRO_DRUMS>>
-	[[nodiscard]] bool isCymbal(size_t lane) const noexcept { return lane > 1 && this->m_colors[lane].isCymbal(); }
+	[[nodiscard]] bool isCymbal(size_t lane) const noexcept { return lane > 1 && this->m_colors[lane - 1].isCymbal(); }
 
 	template <typename = std::enable_if<PRO_DRUMS>>
-	void toggleCymbal(size_t lane) { if (lane > 1) this->m_colors[lane].toggleCymbal(); }
+	void toggleCymbal(size_t lane) { if (lane > 1) this->m_colors[lane - 1].toggleCymbal(); }
 
 	template <typename = std::enable_if<PRO_DRUMS>>
-	void setCymbal(size_t lane, bool enable) { if (lane > 1) this->m_colors[lane].setCymbal(enable); }
+	void setCymbal(size_t lane, bool enable) { if (lane > 1) this->m_colors[lane - 1].setCymbal(enable); }
 };
