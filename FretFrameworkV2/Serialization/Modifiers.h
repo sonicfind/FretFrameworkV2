5f5ddc570583b3eba8714161cf967c766cab8d32
sonicfind <sonicfind2@gmail.com>
2023-04-28 01:13:29 -0500

    Modifier: change location and update constructors
    Utilize perfect forwarding to allow a single constructor to implement most valid types. (float[2] only exception)
    
    + Re-removes set() functions.

--- FretFrameworkV2/Serialization/Modifiers.h
+++ FretFrameworkV2/Serialization/Modifiers.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <fstream>
-#include <assert.h>
 #include "Types/UnicodeString.h"
-#include <variant>
 
 namespace Modifiers
 {
@@ -25,8 +23,43 @@ namespace Modifiers
 		Type m_type = Type::NONE;
 		alignas(UnicodeString) char m_BUFFER[sizeof(UnicodeString)];
 	public:
-		constexpr Modifier(std::string_view name) : m_name(name) {}
-		constexpr Modifier(const Modifier& other) noexcept : m_name(other.m_name), m_type(other.m_type)
+
+		template <typename T>
+		constexpr Modifier(std::string_view name, T&& value) : m_name(name)
+		{
+			using base_T = std::remove_const_t<std::remove_reference_t<T>>;
+			static_assert (std::is_same<base_T, std::u32string>::value ||
+				           std::is_same<base_T, UnicodeString>::value ||
+				           std::is_same<base_T, uint32_t>::value ||
+				           std::is_same<base_T, int32_t>::value ||
+				           std::is_same<base_T, uint16_t>::value ||
+				           std::is_same<base_T, bool>::value ||
+				           std::is_same<base_T, float>::value, "Type is not allowed in a modifier");
+
+			if constexpr (std::is_same<base_T, std::u32string>::value)
+				m_type = Type::STRING_NOCASE;
+			else if constexpr (std::is_same<base_T, UnicodeString>::value)
+				m_type = Type::STRING;
+			else if constexpr (std::is_same<base_T, uint32_t>::value)
+				m_type = Type::UINT32;
+			else if constexpr (std::is_same<base_T, int32_t>::value)
+				m_type = Type::INT32;
+			else if constexpr (std::is_same<base_T, uint16_t>::value)
+				m_type = Type::UINT16;
+			else if constexpr (std::is_same<base_T, bool>::value)
+				m_type = Type::BOOL;
+			else
+				m_type = Type::FLOAT;
+
+			new(m_BUFFER) base_T(std::forward<T>(value));
+		}
+
+		constexpr Modifier(std::string_view name, float flt1, float flt2) : m_name(name), m_type(Type::FLOATARRAY)
+		{
+			new(m_BUFFER) float[2](flt1, flt2);
+		}
+
+		Modifier(const Modifier& other) noexcept : m_name(other.m_name), m_type(other.m_type)
 		{
 			if (m_type == Type::STRING)
 				new(m_BUFFER) UnicodeString(other.cast<UnicodeString>());
@@ -41,99 +74,6 @@ namespace Modifiers
 			memcpy(m_BUFFER, other.m_BUFFER, sizeof(m_BUFFER));
 			other.m_type = Type::NONE;
 		}
-
-		constexpr void set(std::u32string&& string)
-		{
-			reset();
-			m_type = Type::STRING_NOCASE;
-			new(m_BUFFER) std::u32string(std::move(string));
-		}
-
-		constexpr void set(UnicodeString&& string)
-		{
-			reset();
-			m_type = Type::STRING;
-			new(m_BUFFER) UnicodeString(std::move(string));
-		}
-
-		constexpr void set(const UnicodeString& string)
-		{
-			reset();
-			m_type = Type::STRING;
-			new(m_BUFFER) UnicodeString(string);
-		}
-
-		constexpr void set(uint32_t u32)
-		{
-			reset();
-			m_type = Type::UINT32;
-			new(m_BUFFER) uint32_t(u32);
-		}
-
-		constexpr void set(int32_t i32)
-		{
-			reset();
-			m_type = Type::INT32;
-			new(m_BUFFER) int32_t(i32);
-		}
-
-		constexpr void set(uint16_t u16)
-		{
-			reset();
-			m_type = Type::UINT16;
-			new(m_BUFFER) uint16_t(u16);
-		}
-
-		constexpr void set(bool bl)
-		{
-			reset();
-			m_type = Type::BOOL;
-			new(m_BUFFER) bool(bl);
-		}
-
-		constexpr void set(float flt)
-		{
-			reset();
-			m_type = Type::FLOAT;
-			new(m_BUFFER) float(flt);
-		}
-
-		constexpr void set(float flt1, float flt2)
-		{
-			reset();
-			m_type = Type::FLOATARRAY;
-			new(m_BUFFER) float[2](flt1, flt2);
-		}
-
-		constexpr void reset()
-		{
-			if (m_type == Type::STRING)
-				destruct<UnicodeString>();
-			else if (m_type == Type::STRING_NOCASE)
-				destruct<std::u32string>();
-			m_type = Type::NONE;
-		}
-
-	private:
-		template <class T>
-		T& cast()
-		{
-			return *reinterpret_cast<T*>(m_BUFFER);
-		}
-
-		template <class T>
-		const T& cast() const
-		{
-			return *reinterpret_cast<const T*>(m_BUFFER);
-		}
-
-		template <class T>
-		void destruct()
-		{
-			reinterpret_cast<T*>(m_BUFFER)->~T();
-		}
-
-	public:
 
 		Modifier& operator=(const Modifier& other) noexcept
 		{
@@ -167,6 +107,26 @@ namespace Modifiers
 				destruct<std::u32string>();
 		}
 
+	private:
+		template <class T>
+		T& cast()
+		{
+			return *reinterpret_cast<T*>(m_BUFFER);
+		}
+
+		template <class T>
+		const T& cast() const
+		{
+			return *reinterpret_cast<const T*>(m_BUFFER);
+		}
+
+		template <class T>
+		void destruct()
+		{
+			reinterpret_cast<T*>(m_BUFFER)->~T();
+		}
+
+	public:
 		constexpr std::string_view getName() const noexcept { return m_name; }
 
 	private:
