fbe215ea8df8dc0664d198041eb5d7eceabb3b89
sonicfind <sonicfind2@gmail.com>
2023-04-30 09:06:41 -0500

    SongLibrary: Implement scanning functionality to the directory traversal

--- FretFrameworkV2/Main.cpp
+++ FretFrameworkV2/Main.cpp
@@ -1,46 +1,22 @@
 #include "Song/Song.h"
-#include "Song Library/Entry/LibraryEntry.h"
+#include "Song Library/SongLibrary.h"
 #include <iostream>
 int main()
 {
-	Song song;
-
+	SongLibrary library;
 	std::string filename;
-	std::cout << "Drag and drop a file:";
+	std::cout << "Drag and drop a Directory:";
 	std::getline(std::cin, filename);
 
 	if (filename[0] == '\"')
 		filename = filename.substr(1, filename.length() - 2);
 
+	std::vector<std::filesystem::path> directories = { filename };
 	auto t1 = std::chrono::high_resolution_clock::now();
-	if (song.load(filename))
-	{
-		auto t2 = std::chrono::high_resolution_clock::now();
-		long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-		std::cout << "Load took " << count / 1000.0 << " milliseconds\n";
-
-		//if (filename.ends_with(".chart") || filename.ends_with(".mid"))
-			//song.save(filename);
-	}
-	else
-		std::cout << "Load failed\n";
-	
-	
-	std::filesystem::directory_entry ini(std::filesystem::path(filename).replace_filename(U"song.ini"));
-	std::filesystem::directory_entry chart(filename);
-
-	LibraryEntry entry(chart.last_write_time());
-	t1 = std::chrono::high_resolution_clock::now();
-	if (ini.exists())
-		entry.readIni(ini.path(), ini.last_write_time());
-	if (entry.scan(filename))
-	{
-		auto t2 = std::chrono::high_resolution_clock::now();
-		long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-		std::cout << "Scan took " << count / 1000.0 << " milliseconds\n";
-	}
-	else
-		std::cout << "Scan failed\n"; 
+	library.scan(directories);
+	auto t2 = std::chrono::high_resolution_clock::now();
+	long long count = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	std::cout << "Full scan took " << count / 1000.0 << " milliseconds\n";
 
 	std::cout << "Press Enter to Exit" << std::endl;
 	std::getline(std::cin, filename);
--- FretFrameworkV2/Song Library/SongLibrary.cpp
+++ FretFrameworkV2/Song Library/SongLibrary.cpp
@@ -1,4 +1,5 @@
 #include "SongLibrary.h"
+#include <iostream>
 
 void SongLibrary::scan(const std::vector<std::filesystem::path>& baseDirectories)
 {
@@ -8,10 +9,65 @@ void SongLibrary::scan(const std::vector<std::filesystem::path>& baseDirectories
 
 void SongLibrary::scanDirectory(const std::filesystem::path& directory)
 {
+	static const std::pair<std::filesystem::path, LibraryEntry::ChartType> CHARTTYPES[] =
+	{
+		{ U"notes.bch",	  LibraryEntry::BCH },
+		{ U"notes.cht",   LibraryEntry::CHT },
+		{ U"notes.mid",   LibraryEntry::MID },
+		{ U"notes.midi",  LibraryEntry::MID },
+		{ U"notes.chart", LibraryEntry::CHT },
+	};
+	static const std::filesystem::path ININAME = U"song.ini";
+
+	std::optional<std::filesystem::directory_entry> charts[5]{};
+	std::optional<std::filesystem::directory_entry> ini;
 	std::vector<std::filesystem::path> directories;
 	for (const auto& file : std::filesystem::directory_iterator(directory))
+	{
+		const std::filesystem::path& path = file.path();
 		if (file.is_directory())
-			directories.push_back(file.path());
+		{
+			directories.push_back(path);
+			continue;
+		}
+
+		const std::filesystem::path filename = path.filename();
+		if (filename == ININAME)
+		{
+			ini = file;
+			continue;
+		}
+
+		for (size_t i = 0; i < std::size(CHARTTYPES); ++i)
+		{
+			if (filename == CHARTTYPES[i].first)
+			{
+				charts[i] = file;
+				break;
+			}
+		}
+	}
+
+	if (!ini)
+	{
+		charts[0].reset();
+		charts[2].reset();
+		charts[3].reset();
+	}
+
+	for (size_t i = 0; i < std::size(CHARTTYPES); ++i)
+	{
+		if (charts[i])
+		{
+			LibraryEntry entry(*charts[i]);
+			if (ini)
+				entry.readIni(*ini);
+
+			LoadedFile file(charts[i]->path());
+			entry.scan(file, CHARTTYPES[i].second);
+			return;
+		}
+	}
 
 	for (const auto& subDirectory : directories)
 		scanDirectory(subDirectory);
