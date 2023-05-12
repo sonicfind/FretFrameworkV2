cb39888124590fcb509ffa6ad97a3f371865eb7a
sonicfind <sonicfind2@gmail.com>
2023-05-12 10:38:19 -0500

    DrumScan_legacy: Fix isComplete() conditions

--- FretFrameworkV2/Song Library/Entry/DrumScan_Legacy.h
+++ FretFrameworkV2/Song Library/Entry/DrumScan_Legacy.h
@@ -10,4 +10,5 @@ public:
 	InstrumentScan(DrumType_Enum type);
 	void setType(DrumType_Enum type);
 	DrumType_Enum getType() const noexcept { return m_type; }
+	bool isComplete() const noexcept { return m_subTracks == 31 && m_type != DrumType_Enum::LEGACY; }
 };
